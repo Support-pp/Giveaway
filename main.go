b1f33febfd0bef8dbd1cf458a1b149479fc7274e
Johannes Waigel <support@allesverhext.de>
2018-12-13 18:32:01 +0100

    Update main.go

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 }
 
 func myHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "giveaway.support-pp.de")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers","Content-Type,access-control-allow-origin, access-control-allow-headers")
 }

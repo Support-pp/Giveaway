a458771abd8c51130f206e8ef77c91f9b244ddcd
Johannes Waigel <support@allesverhext.de>
2018-12-13 15:25:11 +0100

    Update main.go

--- main.go
+++ main.go
@@ -22,5 +22,13 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/new", HandleQuestion).Methods("POST")
 	router.HandleFunc("/api/internal", VerifyRequest).Methods("POST")
+	router.HandleFunc("/api/new", setupResponse).Methods("OPTIONS")
+	router.HandleFunc("/api/internal", setupResponse).Methods("OPTIONS")
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+}
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
